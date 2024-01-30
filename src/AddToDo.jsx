b8a9e96b35b9330dd22f6a9cc3ab4ce9a28f66f7
duoni-o <57888009+duoni-o@users.noreply.github.com>
2024-01-30 17:44:46 +0900

    React를 활용한 ToDoList UI 변경, 데이터 삽입 실습

---
+++ src/AddToDo.jsx
@@ -0,0 +1,67 @@
+import React from "react"
+
+import {
+    TextField,
+    Paper,
+    Button,
+    Grid
+} from "@material-ui/core"
+
+class AddToDo extends React.Component {
+    constructor(props) {
+        super(props);
+
+        // App.js 파일에서 넘겨준 메서드를 add에 저장
+        this.add = props.add;
+
+        // 입력한 문자열을 저장하기 위한 속성을 생성
+        this.state = {item: {title:""}}
+    }
+
+    // TextField에서 값을 입력할 때마다 호출되는 이벤트리스너
+    onInputChange = (e) => {
+        // state.title에 입력한 내용을 대입
+        const thisItem = this.state.item
+        thisItem.title = e.target.value;
+        this.setState({item:thisItem});
+        console.log(this.state.item.title) // 로그 찍기
+    }
+
+    // + 버튼을 눌렀을 때 호출되는 이벤트 리스너
+    onButtonClick = (e) => {
+        // 배열에 데이터를 삽입하는 함수를 호출
+        this.add(this.state.item);
+        // 텍스트 필드를 초기화
+        this.setState({item:{title:""}})
+    }
+
+    // Enter를 누르면 호출되는 이벤트 리스너 (trigger)
+    // 우리 나라에선 이거 만들어 주는 게 좋음
+    enterKeyEventHandler = (e) => {
+        if (e.key === "Enter") {
+            this.onButtonClick();
+        }
+    }
+
+    render() {
+        return (
+            <Paper style = {{margin: 16, padding: 16}}>
+                <Grid container>
+                    <Grid xs = {11} md = {11} item style = {{paddingRight: 16}}>
+                        <TextField placeholder = "여기에 작성하세요" fullWidth
+                        onChange = {this.onInputChange} value = {this.state.item.title}
+                        onKeyPress = {this.enterKeyEventHandler}
+                        />
+                    </Grid>
+                    <Grid xs = {1} md = {1} item>
+                        <Button fullWidth color = "secondary" onClick={this.onButtonClick}>
+                            +
+                        </Button>
+                    </Grid>
+                </Grid>
+            </Paper>
+        )
+    }
+}
+
+export default AddToDo;
--- src/App.js
+++ src/App.js
@@ -4,6 +4,9 @@ import React from "react"
 import ToDo from "./ToDo"
 // import Sample from "./Sample"
 
+import {Paper, List, Container} from "@material-ui/core"
+import AddToDo from "./AddToDo"
+
 class App extends React.Component {
   constructor(props) {
     // App.js에서 넘겨준 속성들을 자신의 props에 저장
@@ -16,17 +19,47 @@ class App extends React.Component {
                         {id:1, "title":"Vue", "done":false},
                         {id:2, "title":"Angular", "done":false}]}
   }
+
+  // 데이터를 추가하는 함수
+  add = (item) => {
+    // react는 props는 수정할 수 없고
+    // state는 수정이 가능하지만 원본을 직접 수정하는 것은 안되고
+    // setState 메서드를 이용해서만 수정 가능
+
+    // 배열에 데이터를 추가하기 위해서 state의 배열을 복사
+    const thisItems = this.state.items;
+
+    // item의 id와 done 값을 설정 - title만 입력하기 때문
+    item.id = "ID_" + thisItems.length;
+    item.done = false;
+
+    // 복사한 데이터에 데이터를 추가
+    thisItems.push(item)
+    // 복사한 데이터를 다시 state에 적용
+    this.setState({items:thisItems});
+  }
+
   render() {
     // 배열을 순회하면서 출력할 내용을 생성
     // item은 배열을 순회할 때 각각의 데이터이고 idx는 인덱스
     // 배열을 순회하면서 출력물을 만들 때는 key를 설정해줘야 함
     // key를 설정하지 않으면 출력에는 문제 없지만 콘솔에 에러 출력됨
-    let display = this.state.items.map((item, idx) => (
-      <ToDo item = {item} key = {item.id} />
-    ));
+    let display = this.state.items.length > 0 && (
+      <Paper style = {{margin:16}}>
+        <List>
+          {this.state.items.map((item, idx) => (
+            <ToDo item = {item} key = {idx} />
+          ))}
+        </List>
+      </Paper>
+    )
+
     return (
       <div className = "App">
-        {display}
+        <Container maxWidth = "md">
+          <AddToDo add = {this.add} />
+          {display}
+        </Container>
       </div>
     )
   }
